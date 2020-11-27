8ace4fb79805e537c6c5c26c171e1c6ee026b8c8
oyenugae <oyenugae@babcock.edu.ng>
2020-11-27 08:22:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+package com.company;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+        String result_row="";
+        for (int i=1; i<=20; i++){
+            int result_dice = (int)(Math.random() * 6)+1;
+
+            if (i % 5 == 0)
+                result_row +=  result_dice+"\n";
+            else
+                result_row +=  result_dice + " ";
+        }
+        
+        JOptionPane.showMessageDialog(null, result_row, "20 Random numbers from 1 to 6", JOptionPane.WARNING_MESSAGE);   }
+}
